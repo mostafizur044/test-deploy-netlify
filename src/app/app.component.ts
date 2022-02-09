8cd1dca488fc0f719726be14c088017f82dabdd9
mostafizur044 <mostafiz.mri@gmail.com>
2022-02-09 11:33:52 +0000

    kttp added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'netlify-demo';
+
+  constructor(private http: HttpClient) {
+    http.get('https://heroku-node-demo-app.herokuapp.com/api').subscribe(data => {
+      console.log(data);
+    });
+  }
 }
